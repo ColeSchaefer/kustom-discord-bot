39312e91cf423e7520b6b8cef603b37e77445617
Cole Schaefer <coleschaefer1@gmail.com>
2018-10-25 16:42:45 -0400

    Added voice kick command.

---
+++ src/commands/voicekick.js
@@ -0,0 +1,30 @@
+const ytdl = require('ytdl-core');
+const settings = require('../../settings.json');
+const strings = require('../strings');
+const audio = require('../audio');
+const translate = require('google-translate-api');
+const fetch = require('node-fetch');
+
+let Command = {
+    Name: 'vkick',
+    Description: 'Kick a user from their voice channel.',
+    RequiredArguments: ['User Mention'],
+    commandCallback: async function(message, bot) {
+	    if (!message.member.voiceChannel) {
+		    return message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "AUDIO_NOT_IN_VOICE_CHANNEL"));
+		}
+		let mention = message.mentions.users.first();
+		if (!mention) return message.channel.send(message.guild.member(message.author) + ' → No user specified.');
+	    let member = message.guild.member(mention);
+        if (message.member.hasPermission('MANAGE_MESSAGES')) {
+            message.guild.createChannel('VOICE_KICK', 'voice').then((channel) => {
+                member.setVoiceChannel(channel).then((user) => {
+                    channel.delete();
+                    let memberUserName = message.guild.member(message.mentions.users.first()).user.username;
+                    message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "AUDIO_USER_VOICE_KICKED") + memberUserName);
+                });
+            });
+        }
+    }
+};
+module.exports = Command;
