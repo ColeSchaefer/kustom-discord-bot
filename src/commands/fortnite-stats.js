b76c5b38fe0a2e22c876151fac5186d865efa984
Cole Schaefer <coleschaefer1@gmail.com>
2018-10-24 21:37:44 -0400

    Added more commands.

--- src/commands/fortnite-stats.js
+++ src/commands/fortnite-stats.js
@@ -12,37 +12,13 @@ let Command = {
           p10: 'Duo',
           p9: 'Squad'
         };
-        async function isXbl(platform) {
-            return (platform == 'xb1' || platform == 'xbl');
-        }
-        async function isPsn(platform) {
-            return (platform == 'ps4' || platform == 'psn');
-        }
-        async function isConsole(platform) {
-            return (isPsn(platform) || isXbl(platform));
-        }
 	    let argv = message.content.substring(settings.prefix.length).split(' ');
         let platforms = ['pc', 'ps4', 'psn', 'xb1', 'xbl'];
         let platform = argv[1];
         
-        if(platforms.indexOf(platform) > -1) {
+        if (platforms.indexOf(platform) > -1) {
             let args = ''; for (let i = 2; i < argv.length; i++) { args += argv[i] + ' '; } args = args.trim();
-            let username;
-            switch(platform) {
-                case 'xbl':
-                case 'xb1':
-                    username = 'xbl(';
-                    break;
-                case 'psn':
-                case 'ps4':
-                    username = 'psn(';
-                    break;
-                default:
-                    username = '';
-                    break;
-            }
-            username += args;
-            if (isConsole(platform)) username += ')';
+            let username; username = args;
             let statsUrl = 'http://api.cschaefer.me/fortnite/tracknet.php'.concat('?username=' + username).concat('&platform=' + platform);
             fetch(statsUrl).then((res) => res.json()).then((stats) => {
                 let fields = [];
@@ -78,72 +54,15 @@ let Command = {
                 
                 let embed = {
                     thumbnail: { url: 'https://api.cschaefer.me/discord/misc/fnbr.png' },
-                    author: { name: stats.epicUserHandle.concat('\'s All-time Stats')},
+                    author: { name: stats.epicUserHandle.concat(strings.GetString(settings.language, "FNSTATS_TITLE"))},
                     fields: fields
                 };
                 message.channel.send({embed});
             });
         // Invalid platform specified.
         } else {
-            
+           
         }
     }
 };
 module.exports = Command;
-
-
-
-// var platform = args[1];
-// 		if (platform != "pc" && platform != "ps4" && platform != "xb1") {
-// 			botCommands.msgCommands.tempMessage(message, botCommands.msgCommands.getEmbed('Please provide a valid platform!'), 3500);
-// 			return;
-// 		}
-// 		var username = ''; for(var i = 2; i < args.length; i++) username += args[i] + ' '; username = username.trim();
-// 		var stats_url = 'http://api.imbypass.pw/fortnite/stats/' + platform + '/' + username.replace(/ /g, '+');
-		
-// 		let plat;
-// 		switch(platform) {
-// 			case 'ps4': plat = 'PlayStation 4'; break;
-// 			case 'xb1': plat = 'Xbox One'; break;
-// 			case 'pc': plat = 'PC/Mac'; break;
-// 			case 'switch': plat = 'Nintendo Switch'; break;
-// 			case 'mobile': plat = 'iOS/Android'; break;
-// 			default: plat = 'Unknown Platform'; break;
-// 		}
-// 		request(stats_url, function(err, res, body) {  
-// 			let stats = JSON.parse(body);
-// 			if(!stats.error) {
-// 				var embed = new Discord.RichEmbed()
-// 					.setAuthor(stats.info.username + '\'s All-time Stats', 'https://image.fnbr.co/pr&ice/icon_vip.png', 'http://tracker.imbypass.pw/player/' + platform + '/' + username.replace(/ /g, '+'))
-// 					.setColor(0xC06EFF)
-// 					.setColor(0xEF4A82)
-// 					.addField('Username:', stats.info.username, true)
-// 					.addField('Platform:', plat, true)
-// 					.addField('Score:', stats.lifetimeStats.score, true)
-// 					.addField('\u200b', '\u200b')
-					
-// 					.addField('Solo Kills:', stats.group.solo.kills, true)
-// 					.addField('Solo Wins:', stats.group.solo.wins, true)
-// 					.addField('Solo K/D Ratio:', (stats.group.solo.kd) + '**%**', true)
-// 					.addField('Duo Kills:', stats.group.duo.kills, true)
-// 					.addField('Duo Wins:', stats.group.duo.wins, true)
-// 					.addField('Duo K/D Ratio:', (stats.group.duo.kd) + '**%**', true)
-// 					.addField('Squad Kills:', stats.group.squad.kills, true)
-// 					.addField('Squad Wins:', stats.group.squad.wins, true)
-// 					.addField('Squad K/D Ratio:', (stats.group.squad.kd) + '**%**', true)
-// 					.addField('\u200b', '\u200b')
-					
-// 					.addField('Total Wins:', stats.lifetimeStats.wins, true)
-// 					.addField('Matches Played:', (stats.lifetimeStats.matches), true)
-// 					.addField('Win Rate:', (stats.lifetimeStats.winrate) + '**%**', true)
-// 					.addField('Total Kills:', stats.lifetimeStats.kills, true)
-// 					.addField('\u200b', '\u200b', true)
-// 					.addField('K/D Ratio:', (stats.lifetimeStats.kd) + '**%**', true)
-					
-// 					.setFooter(stats.info.username + ' (' + stats.info.accountId + ')')
-// 					.setTimestamp()
-// 				message.author.send(embed);
-// 			} else {
-// 				botCommands.msgCommands.tempMessage(message, botCommands.msgCommands.getEmbed('Unable to parse information for player "' + username + '"'), 3500);
-// 			}
-// 		});
---
+++ src/commands/unmute.js
@@ -0,0 +1,52 @@
+const settings = require('../../settings.json');
+const strings = require('../strings');
+
+let Command = {
+    Name: 'unmute',
+    Description: 'Unmute a user in the Discord server.',
+    RequiredArguments: ['User Mention'],
+    commandCallback: async function(message, bot) {
+	    let argv = message.content.substring(settings.prefix.length).split(' ');
+        let userTo = message.guild.member(message.mentions.users.first());
+        
+        if (argv[1] && userTo) {
+            if (message.member.hasPermission('MANAGE_MESSAGES')) { 
+    		    
+    		    // Find the Muted Role
+    		    var role = message.guild.roles.find((x) => x.name === 'Muted');
+    			// Create it if it doesn't exist
+    			if (!role) {
+    				try {
+    				    // Create the Muted role
+    					role = await message.guild.createRole({
+    						name: 'Muted',
+    						color: '#000000',
+    						permissions: []
+    					});
+    				} catch(ex) {
+    					console.log(ex.stack);
+    				}
+    			}
+    			
+				// Set the role's permissions
+				message.guild.channels.forEach(async (channel, id) => {
+					await channel.overwritePermissions(role, {
+						SEND_MESSAGES: false,
+						ADD_REACTIONS: false,
+						CONNECT: false,
+						SPEAK: false
+					});
+				});
+				
+    			if (!userTo.roles.has(role.id)) return message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "USER_ALREADY_UNMUTED"));
+    			if (userTo.id === message.author.id) return message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "USER_CANNOT_MUTE_SELF"));
+    			if (userTo.highestRole.position >= message.member.highestRole.Position) return message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "USER_CANNOT_MUTE_SAME_ROLE_OR_HIGHER"));
+    			
+    			await userTo.removeRole(role);
+    			
+    			message.channel.send(message.guild.member(message.author) + ' → ' + strings.GetString(settings.language, "USER_HAS_BEEN_UNMUTED"));
+    		}
+        }
+    }
+};
+module.exports = Command;
